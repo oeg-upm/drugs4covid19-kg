30681b7ac6361f8184f12aa11db0bd75feaa4b8f
David Chaves <dachafra@gmail.com>
2020-04-05 11:22:21 +0200

    change to pandas

--- normalization/normalize.py
+++ normalization/normalize.py
@@ -2,6 +2,7 @@
 import argparse
 import sys
 import csv
+import pandas
 csv.field_size_limit(sys.maxsize)
 
 def normalization():
@@ -10,58 +11,29 @@ def normalization():
     parser.add_argument("-f", "--file", required=True, help="file to normalize")
     parser.add_argument("-c", "--column", required=True, help="column with multiple values")
     args = parser.parse_args()
-    data = []
-    if len(sys.argv) == 5:
-        try:
-            with open(args.file, newline='') as csv_file:
-                file = csv.reader(csv_file)
-                for row in file:
-                    data.append(row)
-            col = str(args.column)
 
-        except ValueError:
-            print("No input the correct arguments, run pip3 normalize.py -h to see the help")
-            sys.exit()
+    if len(sys.argv) == 5:
+        data = pandas.read_csv(args.file)
+        col = str(args.column)
+
     else:
         print("No input the correct arguments, run pip3 normalize.py -h to see the help)")
         sys.exit()
 
     i = 0
-    index = 0
-    flag = False
-    while i < len(data[0]):
-        if col == data[0][i]:
-            index = i
-            flag = True
+    output_file = "id," + col + "\n"
+    for row in data[col]:
+        if isinstance(row,str):
+            for value in row.split():
+                output_file += str(i) + "," + str(value) + "\n"
+        data[col] = i
         i = i+1
 
-    i = 0
-    print(len(data))
-    if flag:
-        output_file = "id,"+col+"\n"
-        for row in range(len(data)):
-            if row != 0:
-                data_row = data[row][index].split()
-                data[row][index] = str(i)
-                for value in data_row:
-                    output_file += str(i)+","+str(value)+"\n"
-            i = i+1
+    data.to_csv(args.file.replace(".csv", "")+"_norm.csv")
 
-        f = open(args.file, "w")
-        print(len(data))
-        for row in data:
-            line = ""
-            for i in range(len(row)):
-                line += '"'+row[i].replace('\n', '')+'",'
-            line = line[:-1] + '\n'
-            f.write(line)
-        f.close()
-
-        f = open(args.file.replace(".csv", "") + "_"+col+".csv", "w")
-        f.write(str(output_file))
-        f.close()
-    else:
-        print("There is not a column "+col+" in the file")
+    f = open(args.file.replace(".csv", "") + "_" + col + ".csv", "w")
+    f.write(str(output_file))
+    f.close()
 
 if __name__ == "__main__":
     normalization()
