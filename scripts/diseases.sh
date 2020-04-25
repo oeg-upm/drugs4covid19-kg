30fa92d06c5d0963f47e38d6fb65e9c1b69ba85a
David Chaves <dachafra@gmail.com>
2020-04-25 18:24:49 +0200

    typo synonyms

--- scripts/diseases.sh
+++ scripts/diseases.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 curl -g "https://librairy.linkeddata.es/solr/diseases/select?q=parent:[*%20TO%20*]&wt=csv&indent=true&start=0&rows=50000000&fl=id,parent" > diseases-parent.csv
-curl -g "https://librairy.linkeddata.es/solr/diseases/select?q=synonym:[*%20TO%20*]&wt=csv&indent=true&start=0&rows=50000000&fl=id,synonym"  > diseases-synonym.csv
+curl -g "https://librairy.linkeddata.es/solr/diseases/select?q=synonyms:[*%20TO%20*]&wt=csv&indent=true&start=0&rows=50000000&fl=id,synonyms"  > diseases-synonyms.csv
 curl -g "https://librairy.linkeddata.es/solr/diseases/select?q=mappings:[*%20TO%20*]&wt=csv&indent=true&start=0&rows=50000000&fl=id,mappings"  > diseases-mappings.csv
 curl -g "https://librairy.linkeddata.es/solr/diseases/select?q=*:*&wt=csv&indent=true&start=0&rows=50000000&fl=id,name_t,level_i" > diseases.csv
 
-declare -a normalization=("parent" "synonym" "mappings")
+declare -a normalization=("parent" "synonyms" "mappings")
 
 for j in "${normalization[@]}"
 do
